0deb5f6fc87f5860be3207c560aa1c1fb08bb30f
Arina <hadson.rey@mail.ru>
2020-04-07 17:51:40 +0300

    выносит из блока filters данные (моки), убирает "лишний" html - создает генерацию нескольких фильтров из одного шаблона

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import {createSiteMenuTemplate} from "./components/site-menu";
-import {createFilterTemplate} from "./components/filter";
+import {createFiltersTemplate} from "./components/filters";
 import {createBoardTemplate} from "./components/board";
 import {createTasksEditTemplate} from "./components/tasks-edit";
 import {createTaskItemTemplate} from "./components/task-item";
 import {createButtonLoadTemplate} from "./components/button-load";
+import {generateFilters} from "./mocks/filters";
 
 const TASK_COUNT = 3;
+const filters = generateFilters();
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -15,7 +17,7 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
-render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createFiltersTemplate(filters), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
 const siteBoardElement = siteMainElement.querySelector(`.board`);
