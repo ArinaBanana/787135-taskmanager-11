6fb03b21bd5662dbf9ae05b08b7e9085a3424c82
Arina <hadson.rey@mail.ru>
2020-05-04 20:25:13 +0300

    реализует методы удаления и добавления задачи в tasksmodel

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -49,6 +49,25 @@ export default class TasksModel {
     return true;
   }
 
+  removeTask(id) {
+    const index = this._tasks.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._tasks = [].concat(this._tasks.slice(0, index), this._tasks.slice(index + 1));
+
+    this._callHandlers(this._dataChangeHandlers);
+
+    return true;
+  }
+
+  addTask(task) {
+    this._tasks = [].concat(task, this._tasks);
+    this._callHandlers(this._dataChangeHandlers);
+  }
+
   _callHandlers(handlers) {
     handlers.forEach((handler) => handler());
   }
